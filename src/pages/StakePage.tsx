b7b2da30863a5fc5645d6cdb08fab967a63c61c3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-21 12:21:53 +0530

    stake page

--- src/pages/StakePage.tsx
+++ src/pages/StakePage.tsx
@@ -1,4 +1,15 @@
 const StakePage = () => {
-  return <div>StakePage</div>;
+  return (
+    <div className="h-screen text-white pt-20">
+      <div className="flex flex-col items-center mt-32">
+        <h1 className=" text-center text-4xl font-bold my-4">
+          Stake Your NFTs
+        </h1>
+        <button className="py-2 px-5 bg-white rounded-xl text-gray-700 font-bold mt-5 ">
+          Connect Wallet
+        </button>
+      </div>
+    </div>
+  );
 };
 export default StakePage;
